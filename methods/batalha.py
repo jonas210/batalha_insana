907bb4ef9c48a028f19988ebf18efca9fdc21f28
João Guilherme <joaogymer4@gmail.com>
2026-01-12 20:26:18 -0300

    small changes

--- methods/batalha.py
+++ methods/batalha.py
@@ -20,5 +20,4 @@ def batalhar(personagem1, personagem2):
         if not personagem1.esta_vivo():
             print(f'\n {personagem1._nome} foi derrotado! {personagem2._nome} foi o vencedor.')
             break
-        print()
         time.sleep(1)
