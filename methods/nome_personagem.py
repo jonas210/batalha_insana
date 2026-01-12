84b2d890e1f732e38a0339c8506253db857512be
João Guilherme <joaogymer4@gmail.com>
2026-01-12 19:51:09 -0300

    mudanças visuais

--- methods/nome_personagem.py
+++ methods/nome_personagem.py
@@ -8,10 +8,10 @@ def definir_personagem(jogador):
 
     while True:
         print(f'\n Escolha uma classe')
-        time.sleep(2)
+        time.sleep(1)
         print("1 - Guerreiro (Vida: 100, Ataque: 20, Defesa: 10)")
         print("2 - Mago (Vida: 70, Ataque: 25, Defesa: 5)")
-        time.sleep(2)
+        time.sleep(1)
         escolha = input("Digite 1 ou 2: ")
 
         if escolha == "1":
