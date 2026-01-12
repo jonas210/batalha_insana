46d71e411dcf2ae697285a6afa782b7fd570861a
João Guilherme <joaogymer4@gmail.com>
2026-01-12 20:28:04 -0300

    pequeno buff no dano

--- models/mago.py
+++ models/mago.py
@@ -1,9 +1,10 @@
 from models.personagem import Personagem
 import random
+import time
 
 class Mago(Personagem):
     def __init__(self, nome):
-        super().__init__(nome, vida=70, ataque=25, defesa=5)
+        super().__init__(nome, vida=70, ataque=35, defesa=5)
 
 
     def atacar(self, outro):
@@ -15,5 +16,6 @@ class Mago(Personagem):
             print(f'💥 Ataque crítico!')
         dano = random.randint(int(dano*0.8), int(dano*1.2))
         print(f'{self._nome} ataca {outro._nome} e causa {dano} de dano!')
+        time.sleep(1)
         print()
         outro.receber_dano(dano)
