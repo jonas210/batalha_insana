b65cb12e7df60a44df4157c0ab6fc0837a725430
João Guilherme <joaogymer4@gmail.com>
2026-01-14 21:54:54 -0300

    adicao do sistema de defesa e magia!

--- main.py
+++ main.py
@@ -1,20 +1,61 @@
 import time
 from methods.batalha import batalhar
-from methods.nome_personagem import definir_personagem
+from methods.nome_personagem import definir_personagem, definir_inimigo
 
 
-heroi1 = definir_personagem("Jogador 1")
-heroi2 = definir_personagem("Jogador 2")
+def pvp():
+    heroi1 = definir_personagem("Jogador 1")
+    heroi2 = definir_personagem("Jogador 2")
+    return heroi1, heroi2
 
-while True:
-    batalhar(heroi1, heroi2)
-    time.sleep(1)
-    continuar = input("\n Quer tentar de novo?: ").lower()
+def pve():
+    heroi = definir_personagem("Jogador")
+    inimigo = definir_inimigo()
+    return heroi, inimigo
 
-    if continuar.startswith("s") and continuar.isalpha():
-        continue
+print("1 - Jogador vs Computador")
+print("2 - Jogador vs Jogador")
 
-    else:
+modo = input("Escolha: ")
+
+if modo == "1":
+
+    heroi, inimigo = pve()
+
+    while True:
+
+        batalhar(heroi, inimigo)
         time.sleep(1)
-        print("Encerrando o programa")
-        break
+        continuar = input("\n Quer tentar de novo?: ").lower()
+
+        if continuar.startswith("s"):
+            heroi, inimigo = pve()
+            continue
+
+        else:
+            print("Encerrando o programa")
+            time.sleep(1)
+            break
+
+elif modo == "2":
+    
+    heroi1, heroi2 = pvp()
+
+    while True:
+
+        batalhar(heroi1, heroi2)
+        time.sleep(1)
+        continuar = input("\n Quer tentar de novo?: ").lower()
+
+        if continuar.startswith("s"):
+            heroi1, heroi2 = pvp()
+            continue
+
+        else:
+            print("Encerrando o programa")
+            time.sleep(1)
+            break
+
+else:
+    print("Opção inválida")
+
