aa14c6391e5443ce78f722c8b40714f05732bb3d
João Guilherme <joaogymer4@gmail.com>
2026-01-14 21:51:56 -0300

    implementação de bot

--- methods/nome_personagem.py
+++ methods/nome_personagem.py
@@ -1,13 +1,15 @@
+from models.personagem import Personagem
 from models.guerreiro import Guerreiro
 from models.mago import Mago
 import time
+import random
 
 def definir_personagem(jogador):
     print(f'\n Criando o heroi de {jogador}')
     time.sleep(1)
     while True:
-        heroi = input("Digite o nome do seu heroi: ")
-        if heroi.isalpha():
+        heroi = input("Digite o nome do seu heroi: ").strip()
+        if heroi.replace(" ", "").isalpha():
             break
         else:
             print("Digite um nome válido, apenas letras sao suportadas.")
@@ -15,8 +17,8 @@ def definir_personagem(jogador):
     while True:
         print(f'\n Escolha uma classe')
         time.sleep(1)
-        print("1 - Guerreiro (Vida: 100, Ataque: 20, Defesa: 10)")
-        print("2 - Mago (Vida: 70, Ataque: 25, Defesa: 5)")
+        print(f"1 - Guerreiro (Vida: {Guerreiro.VIDA}, Ataque: {Guerreiro.ATAQUE}, Defesa: {Guerreiro.DEFESA}, Mana: {Guerreiro.MANA_MAX})")
+        print(f"2 - Mago (Vida: {Mago.VIDA}, Ataque: {Mago.ATAQUE}, Defesa: {Mago.DEFESA}, Mana: {Mago.MANA_MAX})")
         time.sleep(1)
         escolha = input("Digite 1 ou 2: ")
 
@@ -28,5 +30,14 @@ def definir_personagem(jogador):
         
         else:
             print("Opção invalida, tente novamente")
+
+
+def definir_inimigo():
+    inimigos = [
+        Guerreiro("Orc"),
+        Mago("Feiticeiro Sombrio")
+    ]
+    return random.choice(inimigos)
+
 
 
