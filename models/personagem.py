a12d22f299ff29c20facd32ae947afbc87d561d0
João Guilherme <joaogymer4@gmail.com>
2026-01-14 21:54:33 -0300

    pequenos ajustes

--- models/personagem.py
+++ models/personagem.py
@@ -2,31 +2,43 @@ import random
 import time
 
 class Personagem:
-    def __init__(self, nome, vida, ataque, defesa):
+    def __init__(self, nome, vida, ataque, defesa, mana):
         self._nome = nome
         self._vida = vida
         self._ataque = ataque
         self._defesa = defesa
+        self._mana = mana
+        self._defendendo = False
+    
+    def defender(self):
+        self._defendendo = True
+        print(f'{self._nome} está se defendendo!')
+        self._defesa *= 2
 
     def atacar(self, outro):
-        dano = self._ataque - outro._defesa
-        if dano < 0:
-            dano = 0
+        dano = max(1, self._ataque - outro._defesa)
         dano = random.randint(int(dano*0.8), int(dano*1.2))
         time.sleep(1)
         print(f'{self._nome} ataca {outro._nome} e causa {dano} de dano!')
         outro.receber_dano(dano)
 
     def receber_dano(self, valor):
+        if self._defendendo:
+            valor = valor // 2
+            self._defendendo = False
+            print(f'{self._nome} defendeu parte do ataque!')
+
         self._vida -= valor
         if self._vida < 0:
             self._vida = 0
         print(f"Agora, {self._nome} tem {self._vida} pontos de vida")
         print()
 
-
     def esta_vivo(self):
         return self._vida > 0
+    
+    def get_nome(self):
+        return self._nome
 
     def __str__(self):
         tipo = type(self).__name__
