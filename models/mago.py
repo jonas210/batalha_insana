b229ca79fe2dc8791b3d156f8e8004b63c5eae49
João Guilherme <joaogymer4@gmail.com>
2026-01-14 21:54:15 -0300

    mais elementos pra classe

--- models/mago.py
+++ models/mago.py
@@ -3,19 +3,39 @@ import random
 import time
 
 class Mago(Personagem):
+    VIDA = 70
+    ATAQUE = 25
+    DEFESA = 5
+    MANA_MAX = 15
+
     def __init__(self, nome):
-        super().__init__(nome, vida=70, ataque=35, defesa=5)
+        super().__init__(nome, self.VIDA, self.ATAQUE, self.DEFESA, self.MANA_MAX)
+        self._mana = self.MANA_MAX
 
 
-    def atacar(self, outro):
-        dano = self._ataque - outro._defesa
-        if dano < 0:
-            dano = 0
-        if random.random() < 0.3:
-            dano *= 2
-            print(f'💥 Ataque crítico!')
-        dano = random.randint(int(dano*0.8), int(dano*1.2))
-        print(f'{self._nome} ataca {outro._nome} e causa {dano} de dano!')
+    def magia(self, outro):
+        if self._mana < 5:
+            print(f'{self._nome} tentou conjurar uma magia, mas estava sem mana!')
+            return
+
+        self._mana -= 5
+        dano_base = max(10, self._ataque - outro._defesa)
+        dano = random.randint(int(dano_base*1.6), int(dano_base*2.0))
+        print(f'{self._nome} conjurou uma bola de fogo gigante e causou {dano} de dano!')
         time.sleep(1)
-        print()
+        print(f'{self._nome} gastou 5 de mana!')
+        print(f'Mana: {self._mana}')
+        time.sleep(1)
         outro.receber_dano(dano)
+
+    def receber_dano(self, valor):
+        if self._defendendo:
+            valor = valor // 2
+            self._defendendo = False
+            print(f'{self._nome} criou uma barreira mágica!')
+            time.sleep(1)
+
+        super().receber_dano(valor)
+
+
+
